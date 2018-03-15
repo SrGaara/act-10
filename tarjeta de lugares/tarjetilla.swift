dd2a02b7d9de3d176248450b2bc303b6aabbbdba
Alumno IDS <alumnoids@iMac-de-IT-Admin.local>
2018-03-15 15:34:28 -0600

    primer intento

---
+++ tarjeta de lugares/tarjetilla.swift
@@ -0,0 +1,13 @@
+//
+//  tarjetilla.swift
+//  tarjeta de lugares
+//
+//  Created by Alumno IDS on 15/03/18.
+//  Copyright © 2018 Alumno IDS. All rights reserved.
+//
+
+import UIKit
+
+class tarjetilla: NSObject {
+
+}
